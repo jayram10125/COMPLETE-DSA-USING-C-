0fae5ddd624a8a389caf5c749808f999d23be3e7
jayram10125 <jayram.10125@gmail.com>
2025-01-05 16:48:03 +0530

    printing array

---
+++ ARRAY/printingArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    // basic way to print the element
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // cool way to print the array
+       for(int i=0;i<5;i++)
+    {
+        cout<<i[arr]<<" ";
+    }
+
+}
