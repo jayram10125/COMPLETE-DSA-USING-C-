c7034bf65b26c680a077beef4bd0ec8dff41526f
jayram10125 <jayram.10125@gmail.com>
2025-01-10 11:03:09 +0530

    basic of string

---
+++ STRINGS/basics.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char str[7]={'a','b','c','d','e'};
+    for(int i=0;i<5;i++)
+    {
+        cout<<str[i]<<" ";
+
+    }
+    cout<<endl;
+    //proof of \addindex'\0
+    cout<<(int)str[5];
+    cout<< endl;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        cout<<str[i]<<" ";
+    }
+    cout<<endl;
+    //another way of printing character array
+    cout<<str;
+
+}
