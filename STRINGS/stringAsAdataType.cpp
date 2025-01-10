d5b4246667c43de39289553e2e15ecf5fac6a32b
jayram10125 <jayram.10125@gmail.com>
2025-01-10 11:18:06 +0530

    string as data type

---
+++ STRINGS/stringAsAdataType.cpp
@@ -0,0 +1,33 @@
+//String as a deta type
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    //declaring the string
+
+    /*
+    string str="jay";
+    cout<<str<<endl;
+    */
+      // indexing
+
+    // cout<<str[0]<<endl;
+
+    //  taking input 
+
+    /*
+    string s;
+    cin>>s;  //ye space ke baad bale content ko accept nahi karega
+    cout<<s<<endl;
+    */
+
+    //taking string input with space
+
+    string s1;
+    getline(cin,s1);
+    cout<<s1;
+
+
+    return 0;
+}
