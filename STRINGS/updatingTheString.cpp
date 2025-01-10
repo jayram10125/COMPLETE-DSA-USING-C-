1235640e47bd492fb980bc23256f9a0ac30d80c1
jayram10125 <jayram.10125@gmail.com>
2025-01-10 11:30:50 +0530

    string is mutable in c++

---
+++ STRINGS/updatingTheString.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    string str="jay";
+    str[1]='o';
+    cout<<str; // it will print "joy" means updation is possible means string is mutable in c++
+}
