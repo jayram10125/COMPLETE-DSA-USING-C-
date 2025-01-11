1f832472dad1c392fc0d4e1cbabe52c23f3e6084
jayram10125 <jayram.10125@gmail.com>
2025-01-11 16:20:09 +0530

    anagram or not

---
+++ STRINGS/anagram.cpp
@@ -0,0 +1,19 @@
+//check the string is anagram or not
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s="physicswallah";
+    string t="phywallahsics";
+
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    if(s==t) cout<<true;
+    else cout<<false;
+
+    
+    
+
+}
