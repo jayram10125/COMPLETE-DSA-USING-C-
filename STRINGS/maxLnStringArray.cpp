d486a2fd51cde1c0d27c0547f58189eb7a47c79e
jayram10125 <jayram.10125@gmail.com>
2025-01-11 17:55:32 +0530

    stoi :) string to integer and stoll :) string to long long

---
+++ STRINGS/maxLnStringArray.cpp
@@ -0,0 +1,11 @@
+//given n strings consisting of digits from 0 to 9 . return the index of string which has maximum  Value .(take 0 based indexing)                               
+
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+using namespace std;
+int main(){
+   string arr[]={"0123"};
+   
+}
--- STRINGS/stoi_And_stoll.cpp
+++ STRINGS/stoi_And_stoll.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 using namespace std;
 int main(){
-   string str = "123456";
+   string str = "123456"; // agar "00000007632546" ye koi number hai to x me sirf 7632546 hi store hoga aage ke zero ko leading zero kahate hain and ye store nahi hota hai
    int x = stoi(str);
    cout<<x;
    
