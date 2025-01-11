74ddbeec015b256c7825b3b88fbc79687da3be90
jayram10125 <jayram.10125@gmail.com>
2025-01-11 18:08:31 +0530

    maximum length string from array

--- STRINGS/maxLnStringArray.cpp
+++ STRINGS/maxLnStringArray.cpp
@@ -6,6 +6,20 @@
 #include<algorithm>
 using namespace std;
 int main(){
-   string arr[]={"0123"};
+   string arr[]={"0123","0023","456","00182","940","002901"};
+   int max=stoi(arr[0]);
+   
+   for(int i=1;i<=5;i++)
+   {
+        int x=stoi(arr[i]);
+        if(x>max)   
+        {
+            max=x;   
+
+        }
+   }
+   cout<<max;
+   
+ 
    
 }
