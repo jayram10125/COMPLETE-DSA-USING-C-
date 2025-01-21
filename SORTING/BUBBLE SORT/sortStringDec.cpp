a9dcc261f652933288605249967fc3602888ff0e
jayram10125 <jayram.10125@gmail.com>
2025-01-21 14:38:23 +0530

    sort the string in reverse order

--- SORTING/BUBBLE SORT/sortStringDec.cpp
+++ SORTING/BUBBLE SORT/sortStringDec.cpp
@@ -14,8 +14,29 @@ int main()
         }
     }
 
-    sort(str.begin(),str.end());//ye inbuilt function se ho gya ise bubble sort ke help se karo
-    cout<<str;
+    //sort(str.begin(),str.end());//ye inbuilt function se ho gya ise bubble sort ke help se karo
+
+    cout<<"before sorting : "<<str;
+    int n=str.size();
+    bool flag=true;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0; j<n-1-i;j++ )
+        {
+            if(str[j]>str[j+1])
+            {
+                swap(str[j],str[j+1]);
+                flag=false;
+
+            }
+        }
+        if(flag==true)
+        {
+            break;
+        }
+    }
+    reverse(str.begin(),str.end());
+    cout<<"\nafter sorting the reverse order string is  : "<<str;
 
     return 0;
 }
