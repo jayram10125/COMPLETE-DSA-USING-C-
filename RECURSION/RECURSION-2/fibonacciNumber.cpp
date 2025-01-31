1375e9844d87f6dd1eba1e4e925970f38d0e9add
jayram10125 <jayram.10125@gmail.com>
2025-01-31 12:28:28 +0530

    fibonacci Series

---
+++ RECURSION/RECURSION-2/fibonacciNumber.cpp
@@ -0,0 +1,17 @@
+//Write a function to calculate the nth fibonacci number using recursion
+
+#include <iostream>
+using namespace std;
+int fibo(int n)
+{
+    if(n==1 || n==2) return 1;
+    return fibo(n-1)+fibo(n-2);
+}
+int main() {
+    int n;
+    cout<<"enter n : ";
+    cin>>n;
+    cout<<fibo(n);
+      
+    return 0;
+}
