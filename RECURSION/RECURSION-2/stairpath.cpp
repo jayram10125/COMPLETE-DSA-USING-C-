707f90a6b4457fe3055b1bc04d683c375edd25eb
jayram10125 <jayram.10125@gmail.com>
2025-01-31 15:12:43 +0530

    stair path

---
+++ RECURSION/RECURSION-2/stairpath.cpp
@@ -0,0 +1,15 @@
+//stair path
+#include <iostream>
+using namespace std;
+int stair(int n)
+{
+    if(n==2) return 2;
+    if(n==1) return 1;
+    return stair(n-1) + stair(n-2);
+    
+}
+int main() {
+    
+    cout<<stair(6);
+    return 0;
+}
