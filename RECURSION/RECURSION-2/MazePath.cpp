dee552838416001af786b832e939ee1d738be7d1
jayram10125 <jayram.10125@gmail.com>
2025-01-31 17:16:44 +0530

    maze path ways printing

--- RECURSION/RECURSION-2/MazePath.cpp
+++ RECURSION/RECURSION-2/MazePath.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int maze(int sr , int sc,int er, int ec ){ // sc->starting column , sr-> satarting row , er-> ending row, ec-> ending column
     if(sr>er || sc>ec) return 0;
-    if(sr==er || sc==ec) return 1;
+    if(sr==er && sc==ec) return 1;
     int rightWays = maze(sr,sc+1,er,ec);
     int downWays = maze(sr+1,sc,er,ec);
     int totalWays =rightWays + downWays;
@@ -11,9 +11,24 @@ int maze(int sr , int sc,int er, int ec ){ // sc->starting column , sr-> satarti
 
 
 }
+void printPath(int sr , int sc,int er, int ec,string s)
+{
+    if(sr>er || sc>ec) return ;
+    if(sr==er && sc==ec) {
+        cout<<s<<endl;
+        return;
+    }
+    printPath(sr,sc+1,er,ec,s+'R');//right
+    printPath(sr+1,sc,er,ec,s+'D');//down
+    
+    
+
+}
 int main()
 {
-    cout<<maze(0,0,2,2);
+    //cout<<maze(0,0,2,2);
+    printPath(1,1,3,3,"");
+
 
 
     return 0;
