493bd3d8a25c81c6ddd7cc18cf3519595188cd2c
jayram10125 <jayram.10125@gmail.com>
2025-02-05 17:12:22 +0530

    print vector using recursion

---
+++ RECURSION/RECURSION-3/abc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool a = true, b = false;
+    
+    cout << (a && b) << endl;  // false (0)
+    cout << (a || b) << endl;  // true (1)
+    cout << !a << endl;        // false (0)
+
+    return 0;
+}
+
+
+
+
+
+
+
