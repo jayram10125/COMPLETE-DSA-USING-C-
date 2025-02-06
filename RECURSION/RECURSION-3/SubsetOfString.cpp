14d1e15e49c0764698b6b0e1d73166e84f7f90d4
jayram10125 <jayram.10125@gmail.com>
2025-02-06 17:24:27 +0530

    Subset of a string

---
+++ RECURSION/RECURSION-3/SubsetOfString.cpp
@@ -0,0 +1,23 @@
+//find all the subset of a string
+#include<iostream>
+#include<string>
+using namespace std;
+void printSubset(string ans, string original){
+    if(original=="")
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    char ch = original[0];
+    
+    printSubset(ans,original.substr(1));
+    printSubset(ans+ch,original.substr(1));
+
+}
+ 
+int main()
+{
+    string str="abc";
+    printSubset("",str);
+    return 0;
+}
