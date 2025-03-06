090b2ba99dfb835f5958fb170bccf13263c83d52
jayram10125 <jayram.10125@gmail.com>
2025-03-06 20:41:49 +0530

    all factors in increasing order

---
+++ PRIME MATH/allFactors2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n=60;
+    for(int i=1;i<sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    for(int i=sqrt(n);i>=1;i--){
+        if(n%i==0) cout<<n/i<<" ";
+    }
+
+    return 0;
+}
