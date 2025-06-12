42f1934d355a640ab4f5cc2527cb0912ed2e3afe
jayram10125 <jayram.10125@gmail.com>
2025-06-12 18:34:44 +0530

    printing linkedlist using function

--- LINKEDLIST/RecursivelyDisplayingLinkedList.cpp
+++ LINKEDLIST/RecursivelyDisplayingLinkedList.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 class Node{
     public:
     int val;
@@ -8,35 +7,27 @@ class Node{
     Node(int val){
         this->val=val;
         this->next=NULL;
-    } 
-
+    }
 };
 void display(Node* head){
     Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->val<<" ";
-        temp=temp->next;
-    }
+    cout<<temp->val<<" ";
+    if(head->next==NULL) return;
+    display(head->next);
 }
-// method 2  -> temp banane ki need hi nahi hai
-// void display(Node* head){
-    
-//     while(head!=NULL){
-//         cout<<head->val<<" ";
-//         head=head->next;
-//     }
-// }
+
 int main(){
     Node* a = new Node(10);
     Node* b = new Node(20);
     Node* c = new Node(30);
     Node* d = new Node(40);
+    Node* e = new Node(50);
 
-    a->next=b;
-    b->next=c;
-    c->next=d;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
 
     display(a);
 
-    return 0;
 }
