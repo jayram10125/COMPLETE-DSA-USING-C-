840e8047cc45f05e7ddf85d2d589d14d46ca08d3
jayram10125 <jayram.10125@gmail.com>
2025-06-15 17:27:07 +0530

    inserting an element at rrequired position

---
+++ LINKEDLIST/insertAtAnyIdx.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+class Node{    // user defined data types
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+
+};
+class LinkedList{  // // user defined data structure
+    public:
+    Node* head;
+    Node* tail;
+    int size;
+    LinkedList(){
+        head=tail=NULL;
+        size=0;
+    }
+    
+    void insertAtEnd(int val){
+        Node* temp = new Node(val);
+        if(size==0) head=tail=temp;
+        else{
+            tail->next=temp;
+            tail=temp;
+        }
+        size++;
+    }
+    
+
+    void insertAtHead(int val){
+        Node* temp = new Node(val);
+        if(size==0) head=tail=temp;
+        else{
+            temp->next=head;
+            head=temp;
+        }
+        size++;
+    }
+    void insertAtIdx(int idx,int val){
+        
+        if(idx<0 || idx>size) cout<<"invalid index";
+        else if(idx==0) insertAtHead(val);
+        else if(idx==size) insertAtEnd(val);
+        else {
+            Node* t = new Node(val);
+            Node* temp = head;
+            for(int  i=1;i<=idx-1;i++){
+                temp=temp->next;
+            }
+            t->next=temp->next;
+            temp->next=t;
+            size++;
+        }
+
+    }
+    void display(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->val<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+
+};
+int main(){
+    LinkedList ll ;
+    ll.insertAtEnd(10); //{10->NULL}
+    ll.display();
+    ll.insertAtEnd(20);//{10->20->NULL}
+    ll.display();
+    ll.insertAtEnd(30);//{10->20->30->NULL}
+    ll.insertAtEnd(40);//{10->20->30->40->NULL}
+    ll.display();
+    
+    ll.insertAtHead(50);//{50->10->20->30->40->NULL}
+    ll.display();
+    ll.insertAtHead(60);//{60->50->10->20->30->40->NULL}
+    ll.display();
+    ll.insertAtIdx(4,80); //{60->50->10->20->80->30->40->NULL}
+    ll.display();
+    ll.insertAtIdx(7,90); //{60->50->10->20->80->30->40->90->NULL}
+    ll.display();
+
+    cout<<ll.size<<endl;
+
+
+    return 0;
+}
